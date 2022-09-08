af5b59548bec2c013682faf8ff0986926fdd95ee
Lucas Inacio <lawlithi@gmail.com>
2022-09-08 20:44:59 -0300

    Integrate front with back

--- components/Movie.tsx
+++ components/Movie.tsx
@@ -4,7 +4,7 @@ const Movie = ({ movie }) => {
     return (
         <div className={styles.grid}>
             <a className={styles.card}>
-                <img src={movie.image} alt={movie.title} style={{ width: 240, height: 300 }}/>
+                <img className={styles.image} src={movie.image} alt={movie.title}/>
                 <h2>{ movie.title }</h2>
                 <p>Description: {movie.description}</p>
                 <p>Votes: {movie.votes}</p>
